a129f443c388770a874bee50b6d8a2f3aa833eff
MaulingMonkey <git@maulingmonkey.com>
2021-10-13 12:29:26 -0700

    pe::FileCharacteristics

--- src/pe/file_header.rs
+++ src/pe/file_header.rs
@@ -16,7 +16,7 @@ pub struct FileHeader {
     pub symbols:                    u32,
     pub nsymbols:                   u32,
     pub optional_header_size:       u16,
-    pub characteristics:            u16,
+    pub characteristics:            FileCharacteristics,
 }
 
 impl From<RawFileHeader> for FileHeader {
@@ -28,7 +28,7 @@ impl From<RawFileHeader> for FileHeader {
             symbols:                value.symbols.into(),
             nsymbols:               value.nsymbols.into(),
             optional_header_size:   value.optional_header_size.into(),
-            characteristics:        value.characteristics.into(),
+            characteristics:        FileCharacteristics::from_bits_truncate(value.characteristics.to_le()),
         }
     }
 }
@@ -46,4 +46,37 @@ pub(crate) struct RawFileHeader {
     pub nsymbols:                   u32le,
     pub optional_header_size:       u16le,
     pub characteristics:            u16le,
+}
+
+
+
+bitflags::bitflags! {
+    #[repr(transparent)]
+    pub struct FileCharacteristics : u16 {
+        const RELOCS_STRIPPED           = 0x0001;
+        const EXECUTABLE_IMAGE          = 0x0002;
+        const LINE_NUMS_STRIPPED        = 0x0004;
+        const LOCAL_SYMS_STRIPPED       = 0x0008;
+
+        const AGGRESSIVE_WS_TRIM        = 0x0010;
+        const LARGE_ADDRESS_AWARE       = 0x0020;
+        const _RESERVED_0040            = 0x0040;
+        const BYTES_REVERSED_LO         = 0x0080;
+
+        const MACHINE_32BIT             = 0x0100;
+        const DEBUG_STRIPPED            = 0x0200;
+        const REMOVABLE_RUN_FROM_SWAP   = 0x0400;
+        const NET_RUN_FROM_SWAP         = 0x0800;
+
+        const SYSTEM                    = 0x1000;
+        const DLL                       = 0x2000;
+        const UP_SYSTEM_ONLY            = 0x4000;
+        const BYTES_REVERSED_HI         = 0x8000;
+    }
+}
+
+impl Default for FileCharacteristics {
+    fn default() -> Self {
+        Self::empty()
+    }
 }
