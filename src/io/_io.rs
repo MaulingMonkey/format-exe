dd9c2e72895912a2ebc3af0df72278830719ccd4
MaulingMonkey <git@maulingmonkey.com>
2023-04-13 19:32:23 -0700

    io: doc (this isn't even used... remove?)

--- src/io/_io.rs
+++ src/io/_io.rs
@@ -1,3 +1,5 @@
+//! Input/Output traits and extensions
+
 #[doc(hidden)] pub use std::io::*;
 
 mod read_at_ext;                        pub use read_at_ext::*;
--- src/io/read_at_ext.rs
+++ src/io/read_at_ext.rs
@@ -6,13 +6,34 @@ use std::convert::*;
 
 
 
+/// Additional convenience methods for [`ReadAt`]
 pub trait ReadAtExt : ReadAt {
+    /// Read `buf.len()` bytes, overwriting `buf`, from offset `*offset`, advancing `*offset` by the same amount.
+    ///
+    /// *   `buf[..]` *may* have been modified on [`io::Error`]
+    /// *   `*offset` will *not* advance on [`io::Error`]
+    ///
+    /// ### Errors
+    /// *   [`io::ErrorKind::UnexpectedEof`] if `*offset` would overflow
+    /// *   [`io::Error`] if the underlying [`ReadAt::read_exact_at`]s failed
     fn read_exact_at_advance(&self, buf: &mut [u8], offset: &mut u64) -> io::Result<()> {
+        let final_offset = offset.checked_add(buf.len() as u64).ok_or_else(|| io::Error::new(io::ErrorKind::UnexpectedEof, "would read past u64::MAX"))?;
         self.read_exact_at(buf, *offset)?;
-        *offset = offset.checked_add(buf.len() as u64).ok_or_else(|| io::Error::new(io::ErrorKind::UnexpectedEof, "read past u64::MAX"))?;
+        *offset = final_offset;
         Ok(())
     }
 
+    /// Append read bytes into `buf`, from `offset`, until `byte` is encountered.
+    ///
+    /// *   The original contents of `buf` are not cleared
+    /// *   `buf` *may* have been appended to on [`io::Error`]
+    /// *   `buf` should end with `byte` on [`Ok`]
+    /// *   Over-reads of `self` may occur (extra data will be discarded)
+    /// *   The seek position of `self` is left indeterminite
+    ///
+    /// ### Errors
+    /// *   [`io::ErrorKind::UnexpectedEof`] if `offset` would overflow
+    /// *   [`io::Error`] if the underlying [`ReadAt::read_at`]s failed
     fn read_until_at(&self, byte: u8, buf: &mut Vec<u8>, offset: u64) -> io::Result<usize> {
         let start = buf.len();
 
@@ -41,6 +62,17 @@ pub trait ReadAtExt : ReadAt {
         Ok(buf.len() - start)
     }
 
+    /// Append read `\0`-terminated string into `buf`, from `offset`.
+    ///
+    /// *   The original contents of `buf` are not cleared
+    /// *   `buf` *may* have been appended to on [`io::Error`]
+    /// *   `buf` will *not* contain the terminal `\0` on [`Ok`] (unless `buf` already ended with `\0` and this read/appended an empty string)
+    /// *   Over-reads of `self` may occur (extra data will be discarded)
+    /// *   The seek position of `self` is left indeterminite
+    ///
+    /// ### Errors
+    /// *   [`io::ErrorKind::UnexpectedEof`] if `offset` would overflow
+    /// *   [`io::Error`] if the underlying [`ReadAt::read_at`]s failed
     fn read_asciiz_at<'b>(&'_ self, buf: &'b mut Vec<u8>, offset: u64) -> io::Result<&'b [u8]> {
         let _ = self.read_until_at(0, buf, offset)?;
         Ok(buf[..].strip_suffix(&[0]).unwrap_or(&buf[..]))
