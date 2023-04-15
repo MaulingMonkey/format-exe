e17b711e26d863c0143eecf8a2785d16db9b5dea
MaulingMonkey <git@maulingmonkey.com>
2023-04-14 20:07:16 -0700

    pe::ImportLookupTableEntry{,32,64}: doc, unpub fn import_by_x details

--- src/pe/import_lookup_table.rs
+++ src/pe/import_lookup_table.rs
@@ -3,23 +3,39 @@ use crate::pe::RVA;
 
 
 
+/// [`ordinal`](Self::ordinal) |
+/// [`name_table_rva`](Self::name_table_rva) |
+/// [`is_eot`](Self::is_eot)
+///
 /// ## References
 /// *   <https://docs.microsoft.com/en-us/windows/win32/debug/pe-format#import-lookup-table>
 pub trait ImportLookupTableEntry {
-    /// `true` if `self.0` == `0` (end of the table
+    /// `true` if `self.0` == `0` (end of the table)
     fn is_eot           (&self) -> bool;
-    fn import_by_ordinal(&self) -> bool;
-    fn import_by_name   (&self) -> bool;
-    fn ordinal          (&self) -> Option<u16>;
+
+    /// <code>Some([RVA])</code> if the function is imported by name (typical)
     fn name_table_rva   (&self) -> Option<RVA>;
+
+    /// <code>Some([u16])</code> if the function is imported by [ordinal/index rather than by name](https://learn.microsoft.com/en-us/cpp/build/exporting-functions-from-a-dll-by-ordinal-rather-than-by-name)
+    fn ordinal          (&self) -> Option<u16>;
 }
 
+/// `impl` [`ImportLookupTableEntry`]:
+/// [`ordinal`](Self::ordinal) |
+/// [`name_table_rva`](Self::name_table_rva) |
+/// [`is_eot`](Self::is_eot)
+///
 /// ## References
 /// *   <https://docs.microsoft.com/en-us/windows/win32/debug/pe-format#import-lookup-table>
 #[repr(transparent)]
 #[derive(Clone, Copy, Default, Debug, PartialEq, Eq)] // TODO: PartialOrd, Ord, Hash
 pub struct ImportLookupTableEntry32(u32le);
 
+/// `impl` [`ImportLookupTableEntry`]:
+/// [`ordinal`](Self::ordinal) |
+/// [`name_table_rva`](Self::name_table_rva) |
+/// [`is_eot`](Self::is_eot)
+///
 /// ## References
 /// *   <https://docs.microsoft.com/en-us/windows/win32/debug/pe-format#import-lookup-table>
 #[repr(transparent)]
@@ -40,10 +56,13 @@ impl FromMemory for ImportLookupTableEntry64 {
     fn from_raw(raw: Self::Raw) -> Result<Self, Self::Error> { Ok(Self(raw)) }
 }
 
-impl ImportLookupTableEntry for ImportLookupTableEntry32 {
-    fn is_eot           (&self) -> bool { self.0 == u32le::new(0) }
+impl ImportLookupTableEntry32 {
     fn import_by_ordinal(&self) -> bool { (self.0.to_le() & (1 << 31)) != 0 }
     fn import_by_name   (&self) -> bool { !self.is_eot() && (self.0.to_le() & (1 << 31)) == 0 }
+}
+
+impl ImportLookupTableEntry for ImportLookupTableEntry32 {
+    fn is_eot(&self) -> bool { self.0 == u32le::new(0) }
 
     fn ordinal(&self) -> Option<u16> {
         if self.import_by_ordinal() {
@@ -62,10 +81,13 @@ impl ImportLookupTableEntry for ImportLookupTableEntry32 {
     }
 }
 
-impl ImportLookupTableEntry for ImportLookupTableEntry64 {
-    fn is_eot           (&self) -> bool { self.0 == u64le::new(0) }
+impl ImportLookupTableEntry64 {
     fn import_by_ordinal(&self) -> bool { (self.0.to_le() & (1 << 63)) != 0 }
     fn import_by_name   (&self) -> bool { !self.is_eot() && (self.0.to_le() & (1 << 31)) == 0 }
+}
+
+impl ImportLookupTableEntry for ImportLookupTableEntry64 {
+    fn is_eot(&self) -> bool { self.0 == u64le::new(0) }
 
     fn ordinal(&self) -> Option<u16> {
         if self.import_by_ordinal() {
